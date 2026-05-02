dd7944d87d26d06d756a71a1464bee92de0388b0
sorki <srk@48.io>
2026-05-02 09:43:56 +0200

    fill in installedSublibs in ConfiguredConversion

--- cabal-install-solver/src/Distribution/Solver/Modular/ConfiguredConversion.hs
+++ cabal-install-solver/src/Distribution/Solver/Modular/ConfiguredConversion.hs
@@ -5,8 +5,11 @@ module Distribution.Solver.Modular.ConfiguredConversion
 import Data.Maybe
 import Prelude hiding (pi)
 import Data.Either (partitionEithers)
+import Data.Set (Set)
+import qualified Data.Set as S
 
 import Distribution.Package (UnitId, packageId)
+import Distribution.Types.InstalledPackageInfo (InstalledPackageInfo(installedSublibs))
 
 import qualified Distribution.Simple.PackageIndex as SI
 
@@ -40,12 +43,12 @@ convCP :: Show loc => SI.InstalledPackageIndex ->
           CP QPN -> ResolverPackage loc
 convCP iidx sidx (CP qpi fa es ds) =
   case convPI qpi of
-    Left  pi ->
+    Left (pi, subPis) ->
                 (if (pkgName . IPI.sourcePackageId . fromJust $ SI.lookupUnitId iidx pi)
                     `elem` (map fst tracedPackages) then tracePrettyId else id) $
                 PreExisting $
                   InstSolverPackage {
-                    instSolverPkgIPI = fromJust $ SI.lookupUnitId iidx pi,
+                    instSolverPkgIPI = addSublibs iidx subPis $ fromJust $ SI.lookupUnitId iidx pi,
                     instSolverPkgLibDeps = fmap fst ds',
                     instSolverPkgExeDeps = fmap snd ds'
                   }
@@ -63,17 +66,29 @@ convCP iidx sidx (CP qpi fa es ds) =
     ds' :: ComponentDeps ([SolverId] {- lib -}, [SolverId] {- exe -})
     ds' = fmap (partitionEithers . map convConfId) ds
 
-convPI :: PI QPN -> Either UnitId PackageId
-convPI (PI _ (I _ (Inst pi))) = Left pi
---- XXX/srk: loosing subPis
-convPI (PI _ (I _ (InstGroup pi _subPis))) = Left pi
+    addSublibs
+      :: SI.InstalledPackageIndex
+      -> Set UnitId
+      -> InstalledPackageInfo
+      -> InstalledPackageInfo
+    addSublibs idx subPis info =
+      info { installedSublibs =
+               catMaybes
+               $ map
+                   (SI.lookupUnitId idx)
+                   (S.toList subPis)
+           }
+
+convPI :: PI QPN -> Either (UnitId, Set UnitId) PackageId
+convPI (PI _ (I _ (Inst pi))) = Left (pi, mempty)
+convPI (PI _ (I _ (InstGroup pi subPis))) = Left (pi, subPis)
 convPI pi                     = Right (packageId (either id id (convConfId pi)))
 
 convConfId :: PI QPN -> Either SolverId {- is lib -} SolverId {- is exe -}
 convConfId (PI (Q (PackagePath _ q) pn) (I v loc)) =
     case loc of
         Inst pi -> Left (PreExistingId sourceId pi)
-        --- XXX/srk: loosing subPis
+        --- XXX/srk: loosing subPis (this is fine)
         InstGroup pi _subPis -> Left (PreExistingId sourceId pi)
         InRepo
           | QualExe _ pn' <- q
