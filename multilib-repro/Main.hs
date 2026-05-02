bdcb9f43b6afed6b5cab3e8404805c4e8cfdc0d9
sorki <srk@48.io>
2026-05-02 09:26:44 +0200

    reproducer: add some code and dummy-main executable

---
+++ multilib-repro/Main.hs
@@ -0,0 +1,6 @@
+module Main where
+
+import qualified Dummy
+
+main :: IO ()
+main = Dummy.main
--- multilib-repro/src/Dummy.hs
+++ multilib-repro/src/Dummy.hs
@@ -1,1 +1,9 @@
 module Dummy where
+
+import Control.Monad.Class.MonadSay
+import Control.Monad.Class.MonadTime.SI
+
+main :: IO ()
+main = do
+  say "Hello installed multilibs"
+  getCurrentTime >>= say . show
